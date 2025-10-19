d34d9e3515f7a1322181611f14454f8fe22ab6d8
Ekateriiinan <volosikwww@gmail.com>
2025-10-19 22:31:59 +0300

    Update parent.cpp

--- parent.cpp
+++ parent.cpp
@@ -20,50 +20,50 @@
 #include "common.h"
 
 int main() {
-    int pipeFD1[2];
-    int pipeFD2[2];
+    int pipe1[2];
+    int pipe2[2];
 
-    if (pipe(pipeFD1) == -1) {
+    if (pipe(pipe1) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
 
-    if (pipe(pipeFD2) == -1) {
+    if (pipe(pipe2) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
 
-    writeString(STDOUT_FILENO, _USER_ALERT_FILE_INPUT);
+    writeString(STDOUT_FILENO, FILE_INPUT);
     
     std::string filename;
     if (readString(STDIN_FILENO, filename) == EOF) {
-        writeString(STDOUT_FILENO, _USER_ALERT_ERROR_FILE);
+        writeString(STDOUT_FILENO, FILE_ERROR);
         exit(EXIT_FAILURE);
     }
 
-#ifdef _WIN32
-    int fileFD = _open(filename.c_str(), _O_CREAT | _O_WRONLY | _O_APPEND, 0644);
-#else
-    int fileFD = open(filename.c_str(), O_CREAT | O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR);
-#endif
+    #ifdef _WIN32
+        int file = _open(filename.c_str(), _O_CREAT | _O_WRONLY | _O_APPEND, 0644);
+    #else
+        int file = open(filename.c_str(), O_CREAT | O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR);
+    #endif
 
-    if (fileFD < 0) {
-        writeString(STDOUT_FILENO, _USER_ALERT_ERROR_FILE);
-        perror("file");
-        exit(EXIT_FAILURE);
-    }
+        if (file < 0) {
+            writeString(STDOUT_FILENO, FILE_ERROR);
+            perror("file");
+            exit(EXIT_FAILURE);
+        }
 
-#ifdef _WIN32
+    #ifdef _WIN32
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
-    
+
     si.dwFlags |= STARTF_USESTDHANDLES;
-    si.hStdInput = (HANDLE)_get_osfhandle(pipeFD1[RD]);
-    si.hStdOutput = (HANDLE)_get_osfhandle(pipeFD2[WR]);
-    si.hStdError = (HANDLE)_get_osfhandle(fileFD);
+    si.hStdInput = (HANDLE)_get_osfhandle(pipe1[RD]);
+    si.hStdOutput = (HANDLE)_get_osfhandle(pipe2[WR]);  
+    si.hStdError = (HANDLE)_get_osfhandle(pipe2[WR]);   
     
     std::string command = "child.exe";
     if (!CreateProcess(NULL, (LPSTR)command.c_str(), NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
@@ -71,70 +71,69 @@ int main() {
         exit(EXIT_FAILURE);
     }
     
-    
-    close(pipeFD1[RD]);
-    close(pipeFD2[WR]);
+    close(pipe1[RD]);
+    close(pipe2[WR]);
     
     while (true) {
-        writeString(STDOUT_FILENO, _USER_ALERT_STRING_INPUT);
+        writeString(STDOUT_FILENO, STRING_INPUT);
+       
         std::string str;
         if (readString(STDIN_FILENO, str) == EOF) {
-            close(pipeFD1[WR]);
+            close(pipe1[WR]);
             break;
         }
-        writeString(pipeFD1[WR], str + "\n");
-        
+        writeString(pipe1[WR], str + "\n");
+
         std::string response;
-        readString(pipeFD2[RD], response);
-        writeString(STDOUT_FILENO, response);
+        readString(pipe2[RD], response); 
+        writeString(file, response);
     }
-    
-    WaitForSingleObject(pi.hProcess, INFINITE);
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
 
-#else
-    pid_t pid = fork();
+    #else
+        pid_t pid = fork();
 
-    if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid == 0) {
-        close(pipeFD2[RD]);
-        close(pipeFD1[WR]);
-        
-        dup2(pipeFD2[WR], STDOUT_FILENO);
-        dup2(pipeFD1[RD], STDIN_FILENO);
-        dup2(fileFD, STDERR_FILENO);
-
-        execl("./child", "./child", NULL);
-        perror("execl");
-        exit(EXIT_FAILURE);
-    } else {
-        close(pipeFD1[RD]);
-        close(pipeFD2[WR]);
-        
-        while (true) {
-            writeString(STDOUT_FILENO, _USER_ALERT_STRING_INPUT);
-            std::string str;
-            if (readString(STDIN_FILENO, str) == EOF) {
-                close(pipeFD1[WR]);
-                break;
-            }
-            writeString(pipeFD1[WR], str + "\n");
-            
-            std::string response;
-            readString(pipeFD2[RD], response);
-            writeString(STDOUT_FILENO, response);
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
         }
-        
-        wait(NULL);
-        close(pipeFD2[RD]);
-    }
-#endif
 
-    close(fileFD);
+        if (pid == 0) {
+            close(pipe2[RD]);
+            close(pipe1[WR]);
+
+            dup2(pipe1[RD], STDIN_FILENO);
+            dup2(pipe2[WR], STDOUT_FILENO); 
+            dup2(pipe2[WR], STDERR_FILENO);  
+
+            execl("./child", "./child", NULL);
+            perror("execl");
+            exit(EXIT_FAILURE);
+        } else {
+            close(pipe1[RD]);
+            close(pipe2[WR]);
+            
+            while (true) {
+                writeString(STDOUT_FILENO, STRING_INPUT);
+                std::string str;
+                if (readString(STDIN_FILENO, str) == EOF) {
+                    close(pipe1[WR]);
+                    break;
+                }
+                writeString(pipe1[WR], str + "\n");
+                
+                std::string response;
+                readString(pipe2[RD], response);
+                writeString(file, response);
+            }
+            
+            wait(NULL);
+            close(pipe2[RD]);
+        }
+    #endif
+
+    close(file);
     return 0;
 }
